93eed35394deefda75e56a87d316b0a0e47a0cce
mitesh <mitesh@freshbits.in>
2021-05-07 20:56:09 +0530

    changes in assignment page

--- resources/views/create_assignement.blade.php
+++ resources/views/create_assignement.blade.php
@@ -3,43 +3,34 @@
 @section("content")
 <div class="container">
     <div class="row border rounded justify-content-center m-5 p-5">
-        <div class="col-sm-4">
             <form method="POST" action="{{route('assignement.store')}}" enctype="multipart/form-data">
                 @csrf
-                <div class="form-group">
-                    <label>Classes Id:
-                        <input type="number" name="classes_id" class="form-control" required>
-                    </label>
+                <div class="form-group my-3">
+                    <label class="form-label">Classes Id:</label>
+                    <input type="number" name="classes_id" class="form-control" required>
                 </div>
-                <div class="form-group">
-                    <label>Topic:
-                        <input type="text" name="topic" class="form-control" required>
-                    </label>
+                <div class="form-group my-3">
+                    <label class="form-label">Topic:</label>
+                    <input type="text" name="topic" class="form-control" required>
                 </div>
-                <div class="form-group">
-                    <label>Description:
-                        <textarea name="description" id=""></textarea>
-                    </label>
+                <div class="form-group my-3">
+                    <textarea name="description" cols="50" id="" placeholder=" Assignement Description..."></textarea>
                 </div>
-                <div class="form-group">
-                    <label>Submission Date:
-                        <input type="date" name="submission_date" class="form-control" required>
-                    </label>
+                <div class="form-group my-3">
+                    <label class="form-label">Submission Date:</label>
+                    <input type="date" name="submission_date" class="form-control" required>
                 </div>
-                <div class="form-group">
-                    <label>Submission Time:
-                        <input type="time" name="submission_time" class="form-control" required>
-                    </label>
+                <div class="form-group my-3">
+                    <label class="form-label">Submission Time:</label>
+                    <input type="time" name="submission_time" class="form-control" required>
                 </div>
-                <div class="form-group">
-                    <label>Maximum Marks:
-                        <input type="text" name="max_marks" class="form-control" required>
-                    </label>
+                <div class="form-group my-3">
+                    <label class="form-label">Maximum Marks:</label>
+                    <input type="text" name="max_marks" class="form-control" required>
                 </div>
-                <div class="form-group">
-                    <label>File:
-                        <input type="file" name="file" class="form-control" required>
-                    </label>
+                <div class="form-group my-3">
+                    <label class="form-label">File:</label>
+                    <input type="file" name="file" class="form-control" required>
                 </div>
                 <br>
                 <div class="d-flex justify-content-between">
--- resources/views/edit_assignement.blade.php
+++ resources/views/edit_assignement.blade.php
@@ -3,49 +3,40 @@
 @section("content")
 <div class="container">
     <div class="row border rounded justify-content-center m-5 p-5">
-        <div class="col-sm-4">
-            <form method="POST" action="{{route('assignement.update',$assignement->id)}}">
-                @csrf
-                @method('PUT')
-                <div class="form-group">
-                    <label>Classes Id:
-                        <input type="number" name="classes_id" class="form-control" value="{{$assignement->classes_id}}" required>
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Topic:
-                        <input type="text" name="topic" class="form-control" value="{{$assignement->topic}}" required>
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Description:
-                        <textarea name="description" id="" rows="5">{{$assignement->description}}</textarea>
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Submission Date:
-                        <input type="date" name="submission_date" class="form-control" value="{{$assignement->submission_date}}" required>
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Submission Time:
-                        <input type="time" name="submission_time" class="form-control" value="{{$assignement->submission_time}}" required>
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Maximum Marks:
-                        <input type="text" name="max_marks" class="form-control" value="{{$assignement->max_marks}}" required>
-                    </label>
-                </div>
-                <br>
-                <div class="d-flex justify-content-between">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    <a href="{{route('assignement.index')}}" class="btn btn-outline-info">
-                        <i class="fa fa-backward"></i> Back To Home
-                    </a>
-                </div>
-            </form>
-        </div>
+        <form method="POST" action="{{route('assignement.update',$assignement->id)}}">
+            @csrf
+            @method('PUT')
+            <div class="form-group my-3">
+                <label class="form-lable">Classes Id:</label>
+                <input type="number" name="classes_id" class="form-control" value="{{$assignement->classes_id}}" required>
+            </div>
+            <div class="form-group my-3">
+                <label class="form-lable">Topic:</label>
+                <input type="text" name="topic" class="form-control" value="{{$assignement->topic}}" required>
+            </div>
+            <div class="form-group my-3">
+                <textarea name="description" cols="50" placeholder=" Assignement Description...">{{$assignement->description}}</textarea>
+            </div>
+            <div class="form-group my-3">
+                <label class="form-lable">Submission Date:</label>
+                <input type="date" name="submission_date" class="form-control" value="{{$assignement->submission_date}}" required>
+            </div>
+            <div class="form-group my-3">
+                <label class="form-lable">Submission Time:</label>
+                <input type="time" name="submission_time" class="form-control" value="{{$assignement->submission_time}}" required>
+            </div>
+            <div class="form-group my-3">
+                <label class="form-lable">Maximum Marks:</label>
+                <input type="text" name="max_marks" class="form-control" value="{{$assignement->max_marks}}" required>
+            </div>
+            <br>
+            <div class="d-flex justify-content-between">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a href="{{route('assignement.index')}}" class="btn btn-outline-info">
+                    <i class="fa fa-backward"></i> Back To Home
+                </a>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
